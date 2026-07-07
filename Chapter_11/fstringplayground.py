49e4ac522e787016906e58e81286c01eed105fc7
Max <m.s.oorschot@live.nl>
2026-07-07 10:43:21 +0200

    started chapter 11

---
+++ Chapter_11/fstringplayground.py
@@ -0,0 +1,10 @@
+from vector2d_v0 import Vector2d
+
+v = Vector2d(2,4)
+
+print(repr(v))
+print(v)
+
+print(f'string representation: {v!s}')
+print(f'repr representation: {v!r}')
+print(f'Self-documenting expression test: {v=}')
