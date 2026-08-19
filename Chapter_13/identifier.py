4269a483bb46dad233aad68dbda2b21c5d73610b
Max <m.s.oorschot@live.nl>
2026-08-19 12:37:06 +0200

    chapter 13

---
+++ Chapter_13/identifier.py
@@ -0,0 +1,14 @@
+a = "MyFolder"
+b = "Demo002"
+c = "2bring"
+d = "my demo"
+
+print(a.isidentifier())
+print(b.isidentifier())
+print(c.isidentifier())
+print(d.isidentifier())
+
+"""The isidentifier() method in Python is used to check whether a given string qualifies as a valid identifier according to the Python language rules.
+ Identifiers are names used to identify variables, functions, classes, and other objects.
+ A valid identifier must begin with a letter (A-Z or a-z) or an underscore (_), 
+ should contain only alphanumeric characters (A-Z, a-z, 0-9) and underscores (_), and should not be a reserved keyword in Python. """
